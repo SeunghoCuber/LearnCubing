8a12b20ca62bd4207232795a9e2538af28bdba19
Seungho Lee <seungho9422@gmail.com>
2019-03-23 21:15:20 -0700

    page four

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function hitOrMiss() {
 	$("#messageform").show();
 }
 
+function validate() {
+	$("#error").html("Thank you for submitting!")
+	var missing = [];
+	if ($("#name").val() == "") {
+		missing.push("name")
+	}
+	if ($("#email").val() == "") {
+		missing.push(" email")
+	}
+	if ($("#message").val() == "") {
+		missing.push(" message")
+	}
+	if (missing.length == 0) {
+		$("#error").html("Thank you for submitting, " + $("#name").val() + "!")
+		$("#error").css("color", "green")
+	}else {
+		$("#error").html("Missing: " + missing)
+		$("#error").css("color", "red")
+	}
+}
